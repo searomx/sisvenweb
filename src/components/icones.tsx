48d74d4acbc79e3cdc90f4bbe3d15851a306c6eb
searomx <searomx@gmail.com>
2025-09-25 23:23:58 -0300

    alteracoes 250925-2324

--- src/components/icones.tsx
+++ src/components/icones.tsx
@@ -6,7 +6,7 @@ export const IconeTruck = () => {
 			viewBox="0 0 24 24"
 			stroke-width="1.5"
 			stroke="currentColor"
-			class="size-6"
+			className="size-6"
 		>
 			<path
 				stroke-linecap="round"
